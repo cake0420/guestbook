538df147330866c03b16e39322ebefc85004705e
soowan <nick6767676767@gmail.com>
2025-04-15 15:02:47 +0900

    first commit

---
+++ src/test/java/com/cake7/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cake7.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
