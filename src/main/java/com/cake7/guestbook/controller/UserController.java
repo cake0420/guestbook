d9e59618cfafebdc0386f526e44a713a32bde0c5
soowan <nick6767676767@gmail.com>
2025-04-15 15:32:29 +0900

    test: 스웨거 테스트

---
+++ src/main/java/com/cake7/guestbook/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.cake7.guestbook.controller;
+
+import com.cake7.guestbook.dto.TestUserDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "User API", description = "사용자 관련 기능 제공")
+@RestController
+@RequestMapping("/api/test/users")
+public class UserController {
+
+    @Operation(summary = "모든 사용자 조회", description = "등록된 모든 사용자의 이름을 조회합니다.")
+    @GetMapping
+    public List<String> getUsers() {
+        return List.of("Alice", "Bob", "Charlie");
+    }
+
+    @PostMapping("/post")
+    @Operation(summary = "사용자 등록")
+    public ResponseEntity<String> createUser(@RequestBody TestUserDTO userDTO) {
+        return ResponseEntity.ok("사용자 등록: " + userDTO.name());
+    }
+}
